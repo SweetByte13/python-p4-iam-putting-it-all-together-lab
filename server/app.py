7e7c66adfa2969591f3623ab129f01e1a70c9549
Tova Yocheved Hillman <hillman.t.y@gmail.com>
2024-06-13 13:51:22 -0400

    done

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session, make_response, jsonify
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 
@@ -8,19 +8,74 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    def post(self):
+        params = request.json
+        try:
+            user = User(username=params.get('username'), image_url=params.get('image_url'), bio=params.get('bio') )
+            user.password_hash = params.get('password')
+            db.session.add(user)
+            db.session.commit()
+            session['user_id'] = user.id
+            return make_response(user.to_dict(), 201)
+        except ValueError as ve:
+            return make_response(jsonify({'error': str(ve)}), 422)
+        except Exception as e:
+            return make_response({"error": f'An error occurred during signup: {str(e)}'}, 422)
 
 class CheckSession(Resource):
-    pass
+    def get (self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = db.session.get(User, user_id)
+            if user:
+                return make_response(user.to_dict(), 200)
+        return make_response({'error': 'Unauthorized: Must login'}, 401)
 
 class Login(Resource):
-    pass
+    def post(self):
+        params = request.json
+        user = User.query.filter_by( username=params.get('username')).first()
+        if not user:
+            return make_response({'error': 'user not found'}, 401)
+        if user.authenticate(params.get('password')):
+            session['user_id']=user.id
+            return make_response(user.to_dict())
+        else:
+            return make_response({'error': 'invalid password' }, 401)
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        user_id = session.get('user_id')
+        if user_id:
+            session['user_id'] = None
+            return make_response({}, 204)
+        return make_response({"error": "Unauthorized: Must login"}, 401)
 
 class RecipeIndex(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            recipes=[recipe.to_dict() for recipe in Recipe.query.all()]
+            return make_response(recipes, 200)
+        return make_response({"error": "Unauthorized: Must login"}, 401) 
+    
+    def post(self):
+        user_id = session.get('user_id')
+        if user_id:
+            data = request.json
+            try:
+                recipe = Recipe(
+                    title=data.get('title'),
+                    instructions=data.get('instructions'),
+                    minutes_to_complete=data.get('minutes_to_complete'),
+                    user_id=user_id
+                )
+                db.session.add(recipe)
+                db.session.commit()
+                return make_response(recipe.to_dict(), 201)
+            except Exception:
+                return make_response({"error": "Recipe is not valid"}, 422)
+        return make_response({"error": "Unauthorized: Must login"}, 401) 
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
--- server/models.py
+++ server/models.py
@@ -6,10 +6,60 @@ from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
-
-    pass
-
+    
+    serialize_only=('id','username', 'image_url', 'bio',)
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+    
+    recipes = db.relationship('Recipe', order_by='Recipe.id', back_populates= 'user', cascade="all, delete-orphan")
+    
+    
+    @property
+    def password_hash(self):
+        self._password_hash
+        raise AttributeError('password_hash is not a readable attribute')
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        self._password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+        
+    def authenticate(self, password):
+        return bcrypt.check_password_hash( self._password_hash, password)
+        
+    @validates('username')
+    def validates_username(self, key, new_username):
+        if not new_username:
+            raise ValueError("Must have a username")
+        elif User.query.filter(User.username == new_username).first():
+            raise ValueError("Username is already in use")
+        return new_username
+            
+    
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String, nullable=False)
+    minutes_to_complete = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     
-    pass
+    user = db.relationship('User', back_populates= 'recipes')
+    
+    @validates('title')
+    def validates_title(self, key, new_title):
+        if not new_title:
+            raise ValueError("Must have a title")
+        return new_title
+        
+    @validates('instructions')
+    def validates_instructions(self, key, new_instructions):
+        if not new_instructions:
+            raise ValueError("Must have instructions")
+        elif len(new_instructions) < 50:
+            raise ValueError("Instructions must be 50 characters long or more.")
+        return new_instructions
